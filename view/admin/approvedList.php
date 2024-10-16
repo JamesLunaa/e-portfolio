99ca5fc3d2169e8c825d7c26e4c2987b460b3a29
Jameszsz <josecynonluna03@gmail.com>
2024-10-16 11:39:08 +0800

    10/16/24

---
+++ view/admin/approvedList.php
@@ -0,0 +1,213 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="icon" type="image/png" href="../icons/stcLogo.png">
+    <title>Approved</title>
+    <script>
+    function confirmLogout() {
+        if (confirm("Are you sure you want to log out?")) {
+            window.location.href = 'logout.php';
+        }
+    }
+    </script>
+</head>
+
+<body>
+    <div class="mainCont d-flex text-white">
+
+        <div class="card cardBorder d-flex flex-column flex-shrink-0"
+            style="width: 280px; min-height: 100vh; background-color: #09191b;">
+            <div class="card-header menuHead">
+                <div class="fs-4 text-center mt-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="54" fill="currentColor"
+                        class="bi bi-list-stars text-white" viewBox="0 0 16 16">
+                        <path
+                            d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5m0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78zM5.048 3.967l-.087.065zm-.431.355A4.98 4.98 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8zm.344 7.646.087.065z" />
+                    </svg>
+                    <strong class="text-white">STCTI Library MS</strong>
+                </div>
+
+            </div>
+            <div class="card-body border-end border-secondary">
+
+                <ul class="fs-5 nav nav-pills flex-column mb-auto">
+                    <li class="nav-item">
+                        <a href="searchBook.php" class="nav-link text-white" aria-current="page">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-house" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+                            </svg>
+                            Book List
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link active">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-list-stars" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5" />
+                                <path
+                                    d="M2.242 2.194a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.256-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53zm0 4a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.255-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53zm0 4a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.255-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53z" />
+                            </svg>
+                            Request Inbox
+                        </a>
+                    </li>
+                    <li>
+                        <a href="borrowedBooks.php" class="nav-link text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-send" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                            </svg>
+                            Borrowed Books
+                        </a>
+                    </li>
+                    <li>
+                        <a href="records.php" class="nav-link text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                            </svg>
+                            Records
+                        </a>
+                    </li>
+                    <li>
+                        <a href="attendance.php" class="nav-link text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                            </svg>
+                            Attendance
+                        </a>
+                    </li>
+                    <li>
+                        <a href="addBook.php" class="nav-link text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                            </svg>
+                            Add / Remove Books
+                        </a>
+                    </li>
+                    <li>
+                        <a href="addUser.php" class="nav-link text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                            </svg>
+                            Add / Remove Users
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-footer border-end border-secondary text-secondary text-center">
+                @Capstone 2024
+            </div>
+
+        </div>
+
+        <!-- content here -->
+        <div class="d-flex min-vh-100" style="width: 200%;">
+
+            <div class="card min-vh-100 cardBorder" style="width: 200%;">
+                <div class="card-header border-secondary">
+                    <div class="dropdown d-flex justify-content-end">
+                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="../icons/stcLogo.png" alt="" width="60" height="55" class="rounded-circle me-2">
+                            <strong class="fs-4">Librarian</strong>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-white text-small shadow" aria-labelledby="dropdownUser1">
+                            <li><a class="dropdown-item" href="changePass.php">Change Password</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#" onclick="confirmLogout()">Sign out</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+                <div class="card-body mainDisplay">
+                    <div class="text-center">
+                        <h1>Approved List</h1>
+                    </div>
+                    <div class="text-center mt-4 mx-4">
+                        <form class="d-flex gap-2" action="approvedList.php" method="post">
+                            <div class="input-group input-group-lg mb-3">
+                                <input name="info" type="text" class="form-control shadow-sm"
+                                    aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"
+                                    placeholder="Search...">
+                            </div>
+
+                            <div class="d-flex gap-2">
+                                <a href=""><button name="search" class="btn btn-primary text-white btn-lg"
+                                        type="submit">Search</button></a>
+                                <a href="approvedList.php"><button type="submit"
+                                        class="btn btn-lg btn-secondary">Reload</button></a>
+                            </div>
+                        </form>
+                    </div>
+
+                    <section class="userList">
+                        <div class="vh-auto">
+                            <div class="text-center">
+                                <a href="requestInbox.php"><button name="search"
+                                        class="mb-3 btn btn-danger text-white btn-lg" type="submit">Return</button></a>
+                            </div>
+
+                            <table class="table table-dark">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Library ID</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Acc No.</th>
+                                        <th scope="col">Request Date</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Remarks</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    <?php
+                                    include('../../model/admin/approvedList.php');
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ view/index.php
@@ -0,0 +1,82 @@
+<?php
+include('control/login.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="view/styles/loginStyles.css">
+    <link rel="icon" type="image/png" href="view/icons/stcLogo.png">
+    <style>
+    body {
+        background-color: #f8f9fa;
+    }
+
+    .card {
+        border: none;
+        border-radius: 15px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    /* Initially hide the not available message */
+    #not-available {
+        display: none !important;
+    }
+
+    @media screen and (max-width: 1024px) {
+        #main-content {
+            display: none !important;
+            /* Hide the main content */
+        }
+
+        #not-available {
+            display: block !important;
+            /* Show the not available message */
+            text-align: center;
+            font-size: 20px;
+            margin-top: 50px;
+        }
+
+        body {
+            background-color: maroon;
+        }
+    }
+    </style>
+</head>
+
+<body>
+
+
+    <div class="container vh-100 d-flex align-items-center justify-content-center">
+        <div id="not-available" class="card pb-4 border w-50 shadow-lg" style="max-width: 400px;">
+            <h1>This website is currently not available on your device.</h1>
+        </div>
+        <div id="main-content" class="card pb-4 border w-50 shadow-lg" style="max-width: 400px;">
+            <div class="card-header p-3 mb-4 text-center text-white">
+                <h4>Library Login</h4>
+            </div>
+            <form action="index.php" method="post" class="px-4 pb-2">
+                <div class="mb-3 text-center">
+                    <img src="view/icons/stcLogo.png" alt="" width="130" height="100" class="rounded-circle mb-3">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label fw-bold">I.D Number</label>
+                    <input type="text" name="user" class="form-control" id="exampleInputEmail1" required>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label fw-bold">Password</label>
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" name="submit" class="btn btn-primary w-75">Sign In</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
